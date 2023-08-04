02bde4e64c7f04acba01094a3e2d7e408c095099
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-04 17:16:27 -0400

    Include Today's Recipe List in the Explore Screen.

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -16,24 +16,14 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Add Today RecipeListView FutureBuilder.
-    // 1
     return FutureBuilder(
-      // 2
       future: mockService.getExploreData(),
-      // 3
       builder: (context, AsyncSnapshot<ExploreData> snapshot){
-        // 4
         if (snapshot.connectionState == ConnectionState.done) {
-          // 5
           final recipes = snapshot.data?.todayRecipes ?? [];
-          // TODO: Replace this with TodayRecipeListView
-          return Center(
-            child: Container(
-              child: const Text('Show TodayRecipeListView'),
-            ),
-          );
+          // Replace this with TodayRecipeListView
+          return TodayRecipeListView(recipes: recipes);
         } else {
-          // 6
           return const Center(
             child: CircularProgressIndicator(),
           );
