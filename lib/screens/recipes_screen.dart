54a74f846cbcbaa5bbdd42f345a32d9dc2dc2628
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-04 23:33:35 -0400

    Create the Recipe's Screen widget.

---
+++ lib/screens/recipes_screen.dart
@@ -0,0 +1,35 @@
+// Gelin Eguinosa
+// 2023
+
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../api/mock_foodsocial_service.dart';
+import '../components/components.dart';
+
+
+class RecipesScreen extends StatelessWidget {
+  // 1
+  final exploreService = MockFoodSocialService();
+
+  RecipesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    return FutureBuilder(
+      // 3
+      future: exploreService.getRecipes(),
+      builder: (context, AsyncSnapshot<List<SimpleRecipe>> snapshot) {
+        // 4
+        if (snapshot.connectionState == ConnectionState.done) {
+          // Add RecipesGridView Here
+          return RecipesGridView(recipes: snapshot.data ?? []);
+        } else {
+          // 6
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
