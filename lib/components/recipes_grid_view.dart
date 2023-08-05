533fb5d10a5ad6d850fb13d9e67b5ae8a44eaa3f
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-04 23:31:30 -0400

    Create the Recipe's Grid View.

---
+++ lib/components/recipes_grid_view.dart
@@ -0,0 +1,44 @@
+// Gelin Eguinosa
+// 2023
+
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import 'recipe_thumbnail.dart';
+
+
+class RecipesGridView extends StatelessWidget {
+  // 1
+  final List<SimpleRecipe> recipes;
+
+  const RecipesGridView({
+    super.key,
+    required this.recipes,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 16,
+      ),
+      // 3
+      child: GridView.builder(
+        // 4
+        itemCount: recipes.length,
+        // 5
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2
+        ),
+        itemBuilder: (context, index) {
+          // 6
+          final simpleRecipe = recipes[index];
+          return RecipeThumbnail(recipe: simpleRecipe);
+        },
+      ),
+    );
+  }
+}
