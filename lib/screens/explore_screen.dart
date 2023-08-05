ea767608bdeb56980a4c975a38d92e900d14789e
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-04 22:14:32 -0400

    Add Friend's Posts to the Explore Screen.

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -15,15 +15,34 @@ class ExploreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Add Today RecipeListView FutureBuilder.
+    // 1
     return FutureBuilder(
+      // 2
       future: mockService.getExploreData(),
+      // 3
       builder: (context, AsyncSnapshot<ExploreData> snapshot){
+        // 4
         if (snapshot.connectionState == ConnectionState.done) {
-          final recipes = snapshot.data?.todayRecipes ?? [];
-          // Replace this with TodayRecipeListView
-          return TodayRecipeListView(recipes: recipes);
+          // 5
+          return ListView(
+            // 6
+            scrollDirection: Axis.vertical,
+            children: [
+              // 7
+              TodayRecipeListView(
+                  recipes: snapshot.data?.todayRecipes ?? [],
+              ),
+              // 8
+              const SizedBox(height: 16),
+              // 9
+              // TODO: Replace this with FriendPostListView.
+              FriendPostListView(
+                friendPosts: snapshot.data?.friendPosts ?? [],
+              ),
+            ],
+          );
         } else {
+          // 10
           return const Center(
             child: CircularProgressIndicator(),
           );
