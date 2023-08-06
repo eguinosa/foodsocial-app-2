8183cce08edff65d01e123e22b50aa86c159e861
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-05 21:48:46 -0400

    Create the Grocery Screen (To Buy).

---
+++ lib/screens/grocery_screen.dart
@@ -0,0 +1,20 @@
+// Gelin Eguinosa
+// 2023
+
+import 'package:flutter/material.dart';
+
+import 'empty_grocery_screen.dart';
+
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Replace with EmptyGroceryScreen.
+    // TODO: Add a scaffold widget.
+    return const EmptyGroceryScreen();
+  }
+
+  // TODO: Add buildGroceryScreen.
+}
