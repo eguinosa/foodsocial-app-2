bf66b795929b44d06ceb0d777b2491b10fdfad62
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-05 23:46:39 -0400

    Utilize MultiProvider to Manage & Update the State of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@
 // 2023
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'foodsocial_theme.dart';
 import 'home.dart';
+import 'models/models.dart';
 
 
 void main() {
@@ -22,7 +24,15 @@ class FoodSocial2 extends StatelessWidget {
     return MaterialApp(
       theme: theme,
       title: 'Foodsocial 2',
-      home: const Home(),
+      // Replace this with MultiProvider.
+      home: MultiProvider(
+        providers: [
+          // 2
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          // TODO: Add GroceryManager Provider.
+        ],
+        child: const Home(),
+      ),
     );
   }
 }
