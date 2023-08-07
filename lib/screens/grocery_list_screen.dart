8856ebe8e610237b60e4acaab6e2560de5529a84
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-07 18:03:03 -0400

    Support using gestures with the List Items.

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -27,15 +27,51 @@ class GroceryListScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           final item = groceryItems[index];
           // TODO: Wrap in a Dismissable.
-          // TODO: Wrap in an InkWell.
-          return GroceryTile(
+          // Wrap in an InkWell.
+          return Dismissible(
             key: Key(item.id),
-            item: item,
-            onComplete: (change) {
-              if (change != null) {
-                manager.completeItem(index, change);
-              }
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              child: const Icon(
+                Icons.delete_forever,
+                color: Colors.white,
+                size: 50.0,
+              ),
+            ),
+            onDismissed: (direction) {
+              manager.deleteItem(index);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('${item.name} dismissed'))
+              );
             },
+            child: InkWell(
+              child: GroceryTile(
+                key: Key(item.id),
+                item: item,
+                onComplete: (change) {
+                  if (change != null) {
+                    manager.completeItem(index, change);
+                  }
+                },
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GroceryItemScreen(
+                      originalItem: item,
+                      onUpdate: (item) {
+                        manager.updateItem(item, index);
+                        Navigator.pop(context);
+                      },
+                      onCreate: (item) {},
+                    ),
+                  ),
+                );
+              },
+            ),
           );
         },
         separatorBuilder: (context, index) {
